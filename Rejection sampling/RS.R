5929cc7089a9974f79db052d57800a6ee7f72881
Alexander Khramov <alexsandr.khramov@gmail.com>
2026-04-04 09:54:16 +0300

    Add adaptive rejection sampling task

---
+++ Rejection sampling/RS.R
@@ -0,0 +1,146 @@
+# Плотность
+dBirnSaund <- function(x) {
+  gamma <- 1
+  (sqrt(x) + sqrt(1/x)) / (2*gamma*x) * dnorm((sqrt(x) - sqrt(1/x)) / gamma)
+}
+
+# ВАЖНО! Если считать log(dBirnSaund(x)), то будут -Inf, хотя всего-то -1000
+log.dBirnSaund<- function(x) {
+  gamma <- 1
+  log(sqrt(x)+1/sqrt(x)) - log(2*gamma*x) + 1/log(2*pi) - (x^2 - 2*x + 1) / (2*x*gamma^2)
+}
+
+# Функция распределения
+pBirnSaund <- function(x) {
+  gamma <- 1
+  pnorm((sqrt(x) - sqrt(1/x)) / gamma)
+}
+
+# line2points <- function(x1, y1, x2, y2) {
+#   function(x) (y1 + (y2-y1)/(x2-x1) * (x-x1))
+# }
+
+
+# Логарифм плотности НЕ выпуклый вверх: у него одна точка перегиба
+# Поэтому параметр \gamma фиксирован и захардкодены некоторые костыли
+
+
+# Точки z --- точки пересечения прямых, точки изломов мажоранты
+# (формула выведена на листочке и переписана в код)
+z <- function(x,h) {
+  j <- 1:(length(x)-3)
+  ((h[j+2] - h[j]) + ((h[j+1] - h[j])/(x[j+1] - x[j]) * x[j] - (h[j+3] - h[j+2])/(x[j+3]-x[j+2])*x[j+2])) / ((h[j+1] - h[j])/(x[j+1] - x[j]) - (h[j+3] - h[j+2])/(x[j+3] - x[j+2]))
+}
+
+# Находит кусочно-линейную мажоранту по наборам x и h
+# Возвращает список из 3х элементов:
+# $corners --- вектор точек излома мажоранты. Если x состоит из k точек, то точек излома 2*(k-2) - 1 
+# $slope --- вектор длины 2*(k-2), состоящий из угловых коэффициентов мажоранты на участках линейности
+# $intercept --- то же самое со свободными коэффициентами
+u <- function(x, h) {
+  k <- length(x)
+  z.k <- z(x, h)
+  
+  corners <- c(rbind(x[2:(k-2)], z.k), x[k-1])
+  
+  j <- cumsum(rep(c(+2, -1), k - 2))
+  
+  slope <- (h[j+1] - h[j]) / (x[j+1] - x[j])
+  intercept <- h[j] - x[j] * slope
+  
+  # Момент хардкодинга из-за того, что логарифм плотности не выпуклый вверх
+  slope[length(slope)] <- -1/2  # h(x) ~ -0.5x, при x -> Inf
+  intercept[length(intercept)] <- -slope[length(slope)] * x[k-1] + h[k-1]  # чтобы проходила через (x_{k-1}, h(x_{k-1}))
+  
+  list(corners=corners, slope=slope, intercept=intercept)
+}
+
+# интеграл мажоранты (вектор интегралов по отрезкам, где мажоранта линейна)
+integrate.u <- function(u.k) {
+  corners <- c(0, u.k$corners, +Inf)
+  j <- 1:(length(corners)-1)
+  (exp(u.k$slope[j]*corners[j+1] + u.k$intercept[j]) - exp(u.k$slope[j]*corners[j] + u.k$intercept[j])) / u.k$slope[j]
+}
+
+# Моделирование n случайных величин из распределения Бирнбаума-Саундерса
+rBirnSaund <- function(n) {
+  k <- 4  # текущее количество точек, по которым строится мажоранта
+  x <- c(0.1, 0.25, 0.7, 0.75)  # точки, в которых был посчитан логарифм плотности
+  h <- log.dBirnSaund(x)  # значение логарифма плотности в точках из x
+  
+  u.k <- u(x,h)
+  integrals <- integrate.u(u.k)
+  corners <- c(0, u.k$corners)
+  
+  res.sample <- numeric(0)
+  while (length(res.sample) < n) {
+    seed <- .Random.seed
+    accepted <- F
+    # Sampling step
+    ## Sample x* from u_k
+    j <- sample(1:length(corners), size=1, prob=integrals)  # выбираем компоненту
+    x.star <- (log(integrals[j] * u.k$slope[j] * runif(1) + exp(u.k$slope[j]*corners[j] + u.k$intercept[j])) - u.k$intercept[j]) / u.k$slope[j]
+    u.k.star <- u.k$slope[j]*x.star + u.k$intercept[j]
+    ## Sample w from Uniform(0,1)
+    w <- runif(1)
+    ## Squeezing test
+    i <- max(which(x < x.star)) |> suppressWarnings()
+    if ((i < k) && (i != -Inf)) {
+      l.k <- h[i]+((h[i+1] - h[i]) / (x[i+1] - x[i])) * (x.star - x[i])
+      if (w <= exp(l.k - u.k.star)) {
+        res.sample <- c(res.sample, x.star)
+        accepted <- T
+      }
+    }
+    ## Rejection test
+    if(!accepted) {
+      h.star <- log.dBirnSaund(x.star)
+      if (w <= exp(h.star - u.k.star)) {
+        res.sample <- c(res.sample, x.star)
+        accepted <- T
+      }
+      # Updating step
+      if (k < 1000 && i < k) {
+        if (i == -Inf) {
+          x <- c(x.star, x)
+          h <- c(h.star, h)
+        }
+        # else if (i == k) {
+        #   x <- c(x, x.star)  # Мы не добавляем точки, больше 0.75 
+        #   h <- c(h, h.star)  # (точка перегиба чуть правее 0.75)
+        # }
+        else {
+          x <- c(x[1:i], x.star, x[(i+1):k])
+          h <- c(h[1:i], h.star, h[(i+1):k])
+        }
+        k <- k + 1
+        u.k <- u(x,h)
+        integrals <- integrate.u(u.k)
+        corners <- c(0, u.k$corners)
+      }
+    }
+  }
+  res.sample
+}
+
+# Для проверки корректности моделирования, построим выборку из p-value и проверим её распределение на равномерность
+# 1000 раз промоделируем выборку размера 1000, на каждом шаге воспользуемся критерием Колмогорова-Смирнова, для получения p-value
+load("pvalue_sample")
+# recalculate <- T
+if (recalculate){
+  set.seed(start.seed <- 42)
+  pval.sample <- numeric(0)
+  for (counter in (1:1000)) {
+    BS.sample <- rBirnSaund(1000)
+    pval.sample <- c(pval.sample, ks.test(BS.sample, y=pBirnSaund)$p.value)
+  }
+  recalculate <- F
+  curr.seed <- .Random.seed
+  save(pval.sample, start.seed, curr.seed, recalculate, file="pvalue_sample")
+}
+# Опять применим критерий Колмогорова-Смирнова для выборки из p-value
+# (если гипотеза верна, то p-value должно иметь распределение U(0,1))
+ks.test(pval.sample, y="punif")
+hist(pval.sample)
+
+
